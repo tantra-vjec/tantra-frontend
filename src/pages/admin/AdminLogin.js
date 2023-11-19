3dde91bf4459af0b726a553ecf05874776983dfa
Vaishnav Krishna <69088196+Appu779@users.noreply.github.com>
2023-11-19 15:33:39 +0530

    added url of cyclic.sh

--- src/pages/admin/AdminLogin.js
+++ src/pages/admin/AdminLogin.js
@@ -15,7 +15,7 @@ const AdminLogin = () => {
 		setData({ ...data, [input.name]: input.value });
 	};
 
-	const url = "https://determined-veil-toad.cyclic.app/login";
+	const url = "https://busy-plum-salmon-shoe.cyclic.app/login";
 	// const url = 'https://tantra-prod-test-s9utur.mo4.mogenius.io/login';
 
 	const handleSubmit = async (e) => {
--- src/pages/register/RegisterB.js
+++ src/pages/register/RegisterB.js
@@ -33,7 +33,7 @@ function RegisterB() {
     };
 
     // const url = "https://tantra.herokuapp.com/register"
-    const url = "https://bored-newt-bonnet.cyclic.app/register";
+    const url = "https://busy-plum-salmon-shoe.cyclic.app/register";
     const navigate = useNavigate();
     const { state } = useLocation();
     const { name, fee, dept, event_id, group } = state;

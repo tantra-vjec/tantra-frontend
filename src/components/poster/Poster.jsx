382942537046859420285243c7deb9063f0cc474
M V Navaneeth <mvnavaneeth5@gmail.com>
2024-11-02 22:47:41 +0530

    Added zoom in animation

--- src/components/poster/Poster.jsx
+++ src/components/poster/Poster.jsx
@@ -1,19 +1,34 @@
-// Poster.js
-import React from "react";
+import React, { useEffect } from "react";
 import { useMediaQuery } from "react-responsive";
-import Desktop from "../../assets/bg/des.png";
 import crtImage from "../../assets/crt_land.png";
 import BouncingGame from "../BouncingGame/BouncingGame";
-
 import "./Poster.css";
 
 function Poster() {
-  let isMobileDevice = useMediaQuery({
+  const isMobileDevice = useMediaQuery({
     query: "(max-device-width: 500px)",
   });
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const vh = window.innerHeight;
+      const scrolled = window.scrollY;
+      const triggerPoint = vh * 0.25; // 25vh trigger point
+      
+      if (scrolled >= triggerPoint) {
+        console.log('Reached 25vh!');
+        document.querySelector('.img-container').classList.add('scale-on-scroll');
+      } else {
+        document.querySelector('.img-container').classList.remove('scale-on-scroll');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className="fill">      
+    <div className="fill">
       <div className="img-container fade-up">
         <img src={crtImage} className="svg-logo" alt="crt image" />
         <div className="bouncing-game-container">

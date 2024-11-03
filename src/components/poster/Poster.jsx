da8606f3d295cc7558ab6efd1453c1322429c045
M V Navaneeth <67640659+NavaneethMv@users.noreply.github.com>
2024-11-03 07:53:58 +0530

    Animation and Changes in event details (#20)
    * Music band component changes
    
    * Added zoom in animation
    
    * Changed event details
    
    ---------
    
    Co-authored-by: Tantra <webteamtantra@gmail.com>

--- src/components/poster/Poster.jsx
+++ src/components/poster/Poster.jsx
@@ -1,16 +1,31 @@
-// Poster.js
-import React from "react";
+import React, { useEffect } from "react";
 import { useMediaQuery } from "react-responsive";
-import Desktop from "../../assets/bg/des.png";
 import crtImage from "../../assets/crt_land.png";
 import BouncingGame from "../BouncingGame/BouncingGame";
-
 import "./Poster.css";
 
 function Poster() {
-  let isMobileDevice = useMediaQuery({
+  const isMobileDevice = useMediaQuery({
     query: "(max-device-width: 500px)",
   });
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const vh = window.innerHeight;
+      const scrolled = window.scrollY;
+      const triggerPoint = vh * 0.25; // 25vh trigger point
+      
+      if (scrolled >= triggerPoint) {
+        console.log('Reached 25vh!');
+        document.querySelector('.img-container').classList.add('scale-on-scroll');
+      } else {
+        document.querySelector('.img-container').classList.remove('scale-on-scroll');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
     <div className="fill">
@@ -19,7 +34,7 @@ function Poster() {
           src={crtImage}
           className="svg-logo max-sm:scale-75"
           alt="crt image"
-        />
+        />\
         <div className="bouncing-game-container">
           <BouncingGame />
         </div>

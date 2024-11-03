d8b2ee2bc0671b1eb1c0e1c0b0727ca007e9d4eb
Ashik Jhonson <ashikjhonson@gmail.com>
2024-11-03 12:19:06 +0530

    Loader

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,33 +1,50 @@
-import { React, useEffect } from "react";
+import { React, useState, useEffect } from "react";
 import Cards from "../../components/cards/Cards";
 import Footer from "../../components/footer/Footer";
 import Navbar from "../../components/navbar/Navbar";
 import Poster from "../../components/poster/Poster";
 import MusicBand from "../../components/musicBand/Musicband";
-import BouncingGame from "../../components/BouncingGame/BouncingGame";
+import Loader from "../../components/Loader/Loader";
 import logo from "../../assets/logo/tantra.gif";
 
 function Home() {
+  const [loading, setLoading] = useState(true);
+
+  // Simulate an API call or some loading process
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); // Set loading to false after 3 seconds
+    }, 5000); // Adjust the timeout as needed
+
+    return () => clearTimeout(timer); // Cleanup timer on unmount
+  }, []);
   useEffect(() => {
     window.scrollTo(0, 0);
   });
+
   return (
-    <div className="bg-black text-[white] font-[Quicksand] home">
-      <Navbar />
-      <section id="home">
-        {/* <img src={logo} /> */}
-        <Poster />
-      </section>
-      <section id="musicBand">
-        <MusicBand />
-      </section>
-      <section id="events">
-        <Cards />
-      </section>
-      <section id="footer">
-        <Footer />
-      </section>
-    </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="bg-black text-[white] font-[Quicksand] home">
+          <Navbar />
+          <section id="home">
+            {/* <img src={logo} /> */}
+            <Poster />
+          </section>
+          <section id="musicBand">
+            <MusicBand />
+          </section>
+          <section id="events">
+            <Cards />
+          </section>
+          <section id="footer">
+            <Footer />
+          </section>
+        </div>
+      )}
+    </>
   );
 }
 

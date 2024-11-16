377cfb10df810cfa6bdfe8b59d86b1116e7b8572
Ashik Jhonson <ashikjhonson@gmail.com>
2024-11-16 20:29:25 +0530

    Contributors data update

--- src/data/contributors/credit.js
+++ src/data/contributors/credit.js
@@ -1,13 +1,16 @@
 import ashi from "./ashi.png";
-import me from "./me.jpeg";
+import adi from "./adi.png";
+import ani from "./ani.png";
+import josh from "./josh.jpg";
+import bdwj from "./bdwj.jpg";
+import me from "./me.jpeg"; // dummy
 
 const teamMembers = [
     {
       id: 1,
       name: "Abhiram K",
-      title: "Web Team",
-      image:
-        me,
+      title: "Web Team Lead",
+      image: me,
       linkedin: "#",
       github: "#",
       instagram: "#",
@@ -16,15 +19,16 @@ const teamMembers = [
       id: 2,
       name: "Adithya Chandran",
       title: "UI/UX",
-      image: me,
-      linkedin: "#",
-      github: "#",
-      instagram: "#",
+      image: adi,
+      linkedin: "https://www.linkedin.com/in/adithya-chandran-v-32203526a/",
+      github: "https://github.com/Adithyachandranv",
+      instagram: "https://www.instagram.com/adithya_chandran_/",
+      behance: "https://www.behance.net/adithyachandran"
     },
     {
       id: 3,
       name: "Ashik Jhonson",
-      title: "Dev",
+      title: "aka Trinity",
       image: ashi,
       linkedin: "https://www.linkedin.com/in/ashikjhonson/",
       github: "https://github.com/ashikjhonson",
@@ -34,8 +38,7 @@ const teamMembers = [
       id: 4,
       name: "Navaneeth MV",
       title: "Dev",
-      image:
-        me,
+      image: me,
       linkedin: "#",
       github: "#",
       instagram: "#",
@@ -44,8 +47,7 @@ const teamMembers = [
       id: 5,
       name: "Bharadwaj",
       title: "Dev",
-      image:
-        me,
+      image: bdwj,
       linkedin: "https://www.linkedin.com",
       github: "https://github.com/bh4radwaj",
       instagram: "https://www.instagram.com",
@@ -54,21 +56,19 @@ const teamMembers = [
       id: 6,
       name: "Joshua ",
       title: "Dev",
-      image:
-        me,
-      linkedin: "#",
-      github: "#",
-      instagram: "#",
+      image: josh,
+      linkedin: "https://www.linkedin.com/in/joshua-sajeev-60132b1ba/",
+      github: "https://github.com/joshua-sajeev",
+      instagram: "https://www.instagram.com/joshua.sajeev/",
     },
     {
       id: 7,
-      name: "Anirudh ",
+      name: "Anirudh Gireesh NP ",
       title: "UI/UX",
-      image:
-        me,
-      linkedin: "#",
-      github: "#",
-      instagram: "#",
+      image: ani,
+      linkedin: "https://www.linkedin.com/in/anirudh-gireesh-n-p-5bb220231/",
+      github: "https://github.com/Judycyberr",
+      instagram: "https://www.instagram.com/agnfreestyle",
     },
   ];
   
